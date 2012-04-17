3900ec0e96076789fdd58366dbeb65d295c7c6c8
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-04-17 15:29:41 -0700

    started leibniz with some comments

---
+++ chap03/leibniz.lisp
@@ -0,0 +1,7 @@
+
+; Leibniz
+; Shows how a production system should be setup so that each time a rule fires, 
+; a little progress is made towards the solution of a relatively complicated
+; problem.
+
+
